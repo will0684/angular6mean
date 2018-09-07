132b9fccebc9f3da0dc515e6cc2b3cf646a72ede
markonnen <jordan.walcaraz@gmail.com>
2018-09-07 19:54:34 -0400

    Set up production build for deployment to Firebase

--- frontend/angular6mean/src/environments/environment.prod.ts
+++ frontend/angular6mean/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBGLuPvpHmJACtLH8M2MAerAjEX6hYrajU',
+    authDomain: 'angular6mean.firebaseapp.com',
+    databaseURL: 'https://angular6mean.firebaseio.com',
+    projectId: 'angular6mean',
+    storageBucket: 'angular6mean.appspot.com',
+    messagingSenderId: '53675189103'
+  }
 };
